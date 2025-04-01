9dfceeb005925ba4fc3c0b7f5b35be2a1aad3531
Vedant Mahajan <darkcoder8311@gmail.com>
2025-04-01 12:03:04 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
+// Make sure the root element exists
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  const newRoot = document.createElement("div");
+  newRoot.id = "root";
+  document.body.appendChild(newRoot);
+}
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
